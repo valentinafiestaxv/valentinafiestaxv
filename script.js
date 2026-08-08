218894e05971c8eaa642a402247433a7d301cfd7
valentinafiestaxv <valentinafiestaxv@gmail.com>
2026-08-09 00:03:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = https://script.google.com/macros/s/AKfycbx9M1IHk7QeI0RIr2s0ZyNKcHR3ntNgBPaSDZqWUKNz25-PLq5fL1J4QL0xwBErZrt6/exec; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx9M1IHk7QeI0RIr2s0ZyNKcHR3ntNgBPaSDZqWUKNz25-PLq5fL1J4QL0xwBErZrt6/exec"; 
 
 const dropZone = document.getElementById('dropZone');
 const fileInput = document.getElementById('fileInput');
